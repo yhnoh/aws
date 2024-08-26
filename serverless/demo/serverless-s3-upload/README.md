3fa9b0c8257bfeabfcdde14d428c0e4e4577c929
yhnoh <tedgom19@gmail.com>
2024-08-26 23:52:47 +0900

    serverless aws s3 파일 다뤄보기

---
+++ serverless/demo/serverless-s3-upload/README.md
@@ -0,0 +1,4 @@
+
+
+[](https://docs.aws.amazon.com/ko_kr/lambda/latest/dg/lambda-typescript.html)
+[](https://docs.aws.amazon.com/ko_kr/sdk-for-javascript/v3/developer-guide/welcome.html)
--- serverless/demo/serverless-s3-upload/handler.ts
+++ serverless/demo/serverless-s3-upload/handler.ts
@@ -1,5 +1,8 @@
+import { APIGatewayProxyEvent } from 'aws-lambda';
 import { Handler } from 'aws-lambda';
-import { ListBucketsCommand, S3Client } from "@aws-sdk/client-s3";
+import { ListBucketsCommand, ListObjectsV2Command, GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import * as path from "node:path";
+
 
 const client = new S3Client({});
 
@@ -17,6 +20,7 @@ export const getBuckets:Handler = async () => {
             };
         }
     }catch (err){
+        console.error(err);
         return {
             statusCode: 500,
             body: JSON.stringify(err)
@@ -24,3 +28,40 @@ export const getBuckets:Handler = async () => {
     }
 }
 
+export const getObjects:Handler = async (event) => {
+
+    console.log(`event = ${event}`);
+
+    const pathParameters = event.pathParameters;
+    const command = new ListObjectsV2Command({
+        Bucket: pathParameters.bucketName
+    });
+
+    try{
+        const response = await client.send(command);
+        if(response){
+            return {
+                statusCode: 200,
+                body: JSON.stringify(response)
+            };
+        }
+    }catch (err){
+        console.error(err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify(err)
+        };
+    }
+}
+
+export const getObject:Handler = async (event, context) => {
+
+
+    return {
+        statusCode: 200,
+        body: {
+            event: event,
+            context: JSON.stringify(context)
+        }
+    };
+}
