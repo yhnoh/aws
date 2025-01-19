cd54c4822d147199697519ea89f6d0750320a19b
yhnoh <tedgom19@gmail.com>
2025-01-19 14:41:27 +0900

    ECS 및 CI/CD 구축을 위한 앱 제작

---
+++ ecs/fargate/app/README.md
@@ -0,0 +1,1 @@
+> https://docs.aws.amazon.com/AmazonECR/latest/userguide/image-push-iam.html
---
+++ ecs/fargate/app/src/main/java/org/example/app/HelloController.java
@@ -0,0 +1,13 @@
+package org.example.app;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
